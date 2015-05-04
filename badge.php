f62d2dc48b6ae07fa6a2519abea57f33f49b2377
mistic100 <contact@git.strangeplanet.fr>
2015-05-04 13:21:40 +0200

    add badge generator

---
+++ badge.php
@@ -0,0 +1,26 @@
+<?php
+
+$file = json_decode(file_get_contents('downloads.json'), true);
+
+switch ($_GET['what'])
+{
+case 'version':
+  $keys = array_keys($file);
+  $version = array_pop($keys);
+  $url = 'https://img.shields.io/badge/stable%20version-' . $version . '-brightgreen.svg';
+  break;
+  
+case 'downloads':
+  $downloads = array_sum($file);
+  if ($downloads >= 1000)
+  {
+    $downloads = number_format($downloads/1000, 1) . 'k';
+  }
+  $url = 'https://img.shields.io/badge/downloads-' . $downloads . '-blue.svg';
+  break;
+
+default:
+  $url = 'https://img.shields.io/badge/undef-error-red.svg';
+}
+
+header('Location: '.$url);
