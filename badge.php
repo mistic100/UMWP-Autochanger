6eff9da427dca8915fcda3ea4c75c6c467aad551
mistic100 <contact@git.strangeplanet.fr>
2015-12-02 20:41:50 +0100

    flat square badges

--- badge.php
+++ badge.php
@@ -7,7 +7,7 @@ switch ($_GET['what'])
 case 'version':
   $keys = array_keys($file);
   $version = array_pop($keys);
-  $url = 'https://img.shields.io/badge/stable%20version-' . $version . '-brightgreen.svg';
+  $url = 'https://img.shields.io/badge/stable%20version-' . $version . '-brightgreen.svg?style=flat-square';
   break;
   
 case 'downloads':
@@ -16,11 +16,11 @@ case 'downloads':
   {
     $downloads = number_format($downloads/1000, 1) . 'k';
   }
-  $url = 'https://img.shields.io/badge/downloads-' . $downloads . '-blue.svg';
+  $url = 'https://img.shields.io/badge/downloads-' . $downloads . '-blue.svg?style=flat-square';
   break;
 
 default:
-  $url = 'https://img.shields.io/badge/undef-error-red.svg';
+  $url = 'https://img.shields.io/badge/undef-error-red.svg?style=flat-square';
 }
 
 header('Location: '.$url);
